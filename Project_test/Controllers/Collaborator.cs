d793090c755027f475a95a21881f161023fe85ce
Johnny Tran <tranjohnny917@gmail.com>
2024-04-22 18:35:57 -0400

    Initial commit

---
+++ Project_test/Controllers/Collaborator.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project_test.Models;
+using System.Diagnostics;
+
+namespace Project_test.Controllers
+{
+    public class Collaborator : Controller
+    {
+        public IActionResult MeetingMinutes_View()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login_View()
+        {
+            return View();
+        }
+        public IActionResult Home_View()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult ViewProjects()
+        {
+            return View();
+        }
+        public IActionResult ActionItems_View()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GoHome()
+        {
+
+            return RedirectToAction("Home_View", "Collaborator");
+        }
+
+        [HttpPost]
+        public IActionResult GoMeetingMinutes()
+        {
+
+            return RedirectToAction("MeetingMinutes_View", "Collaborator");
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Project_test/Controllers/ExternalCollaboratorController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project_test.Models;
+using System.Diagnostics;
+
+namespace Project_test.Controllers
+{
+    public class ExternalCollaboratorController : Controller
+    {
+        private readonly ILogger<ExternalCollaboratorController> _logger;
+
+        public ExternalCollaboratorController(ILogger<ExternalCollaboratorController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult MeetingMinutes_View()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login_View()
+        {
+            return View();
+        }
+        public IActionResult Home_View()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult GoHome()
+        {
+            
+            return RedirectToAction("Home_View", "ExternalCollaborator");
+        }
+
+        [HttpPost]
+        public IActionResult GoMeetingMinutes()
+        {
+
+            return RedirectToAction("MeetingMinutes_View", "ExternalCollaborator");
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
