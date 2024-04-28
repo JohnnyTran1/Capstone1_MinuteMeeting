8b5a551d4038c01583e93795a98b4712f7b010ff
Aaron Kolker <aarocka@users.noreply.github.com>
2024-04-28 15:37:31 -0400

    Fixed nav bars, footers, and the admin pages

---
+++ Project_test/Controllers/AdminController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_test.Controllers
+{
+	public class AdminController : Controller
+	{
+		public IActionResult CreateProject()
+		{
+			return View();
+		}
+
+		public IActionResult EditProject()
+		{
+			return View();
+		}
+		public IActionResult ViewProjects()
+		{
+			return View();
+		}
+
+		public IActionResult ManageProject()
+		{
+			return View();
+		}
+
+		public IActionResult CreateUser()
+		{
+			return View();
+		}
+		public IActionResult MeetingMinutes_View()
+		{
+			return View();
+		}
+
+		public IActionResult Login_View()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult GoHome()
+		{
+
+			return RedirectToAction("ViewProjects", "Admin");
+		}
+
+		[HttpPost]
+		public IActionResult GoMeetingMinutes()
+		{
+
+			return RedirectToAction("MeetingMinutes_View", "Admin");
+		}
+	}
+}
